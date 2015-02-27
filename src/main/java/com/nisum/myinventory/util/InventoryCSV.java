0db7a7bb8e6f8c4466e73071b5585135b2993534
Cristian Parra <cristian@fision.cl>
2015-02-27 17:36:43 -0300

    End of first history.

--- src/main/java/com/nisum/myinventory/util/InventoryCSV.java
+++ src/main/java/com/nisum/myinventory/util/InventoryCSV.java
@@ -31,7 +31,7 @@ public class InventoryCSV implements Inventory{
 		try {
 			Appendable out = new FileWriter(FILE_NAME);
 			CSVPrinter p = new CSVPrinter(out, CSVFormat.EXCEL);
-			
+
 			for (Item i : items) {
 				p.print(i.getSerialNumber());
 				p.print(i.getDescription());
@@ -44,13 +44,12 @@ public class InventoryCSV implements Inventory{
 			log.error(e.getMessage());
 		}
 	}
-	
 
 	public Item get(Long sNumber) {
 		return find(sNumber);
 	}
 
-	private static Item find(Long sn) {
+	private Item find(Long sn) {
 		Item item = null;
 
 		try {
@@ -60,51 +59,29 @@ public class InventoryCSV implements Inventory{
 
 			for (CSVRecord r : rd) {
 				if(Long.parseLong(r.get(0)) == sn) {
-					item = parseRecord2Item(r);
+					item = this.parseRecord2Item(r);
 				}
 			}
 			in.close();
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 
+		}
 
 		return item;
 	}
 
-	private static Item parseRecord2Item(CSVRecord r) throws StorageCSVItemParseException {
+	private Item parseRecord2Item(CSVRecord r) throws StorageCSVItemParseException {
 		SimpleDateFormat sdf = new SimpleDateFormat("YYYYMMDD");
 		Item item = new Item();
 
 		try {
 			item.setSerialNumber(Long.parseLong(r.get(0)));
 			item.setDescription(r.get(1));
-			item.setBuyDate(sdf.parse(r.get(2)));			
+			item.setBuyDate(sdf.parse(r.get(2)));
 		} catch (ParseException p) {
-			throw new StorageCSVItemParseException();
+			throw new StorageCSVItemParseException(p);
 		}
 
 		return item;
 	}
-
-	// I get it wrong from here:	
-	/*public void create(Item item) throws InventoryItemAlreadyExistException {
-		if(find(item.getSerialNumber()) != null) throw new InventoryItemAlreadyExistException();
-		//persist
-	}
-
-	public void update(Item item) throws InventoryItemNotFoundException {
-		if(find(item.getSerialNumber()) == null) throw new InventoryItemNotFoundException();
-
-		delete(item.getSerialNumber());
-
-		try {
-			create(item);
-		} catch (InventoryItemAlreadyExistException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void delete(Long sn) {
-		//TODO: Add logic here
-	}*/
 }
